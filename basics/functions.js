f4b2791de9e523ffc04194da0d834ad973e1405e
Jenin <110290773+jenin07@users.noreply.github.com>
2024-07-11 19:42:52 +0000

    functions

---
+++ basics/functions.js
@@ -0,0 +1,29 @@
+function sayname() {
+    console.log("hello")
+}
+
+// sayname () // without small bracket it will be reference and with small bracket it will be execution
+
+// function addnum (num1, num2){
+//     console.log (num1 + num2);
+// }
+
+// function addnum (num1, num2){
+//     return (num1 + num2);
+// }
+// const result = addnum(3,7)
+// console.log(result)
+
+
+function loginuser(username)
+{
+    if (username === undefined)
+    {
+        console.log("Please enter your name")
+        return
+    }
+        return `${username} just logged in`
+    
+}
+
+console.log(loginuser("Jenin"))
