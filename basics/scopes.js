880c2d415e66a45f75e2b0871fe67f6e9599cc34
Jenin <110290773+jenin07@users.noreply.github.com>
2024-07-15 18:32:01 +0000

    scopes

--- basics/scopes.js
+++ basics/scopes.js
@@ -1,1 +1,26 @@
- 
+ function one (){
+    const username = "jenin"
+    console.log("inner")
+
+    function two (){
+        const web = "yt"
+        console.log(username )
+    }
+    // trying to call object web outside of its scope
+    // console.log(web)
+
+    two()
+ }
+ one();
+
+
+ function addone(num){
+    return num +2
+ }
+ addone(3)
+
+
+ const addtwo = function (num){
+    return num + 4
+ }
+ addtwo(5)
