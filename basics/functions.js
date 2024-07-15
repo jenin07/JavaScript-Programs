c7ffb547bf7164336343dd69b2fdaa7200fd2b00
Jenin <110290773+jenin07@users.noreply.github.com>
2024-07-15 08:22:23 +0000

    functions

--- basics/functions.js
+++ basics/functions.js
@@ -1,6 +1,6 @@
-function sayname() {
-    console.log("hello")
-}
+// function sayname() {
+//     console.log("hello")
+// }
 
 // sayname () // without small bracket it will be reference and with small bracket it will be execution
 
@@ -14,8 +14,44 @@ function sayname() {
 // const result = addnum(3,7)
 // console.log(result)
 
-function calcartprice(...num1){
-    return num1
-}
 
-console.log(calcartprice(2, 4, 5))
+// rest operator
+
+// function calcartprice(val1, val2, ...num1){
+//     return num1
+// }
+
+// console.log(calcartprice(2, 4, 5))
+
+// const user = {
+//     username: "jenin",
+//     price: 199
+// }
+
+// function handleobject(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+// }
+
+// first method to pass object in function
+// handleobject(user)
+
+// second method to pass object in function
+// handleobject({
+//     username: "john",
+//     price: "5316"
+// })
+
+// how to call array in function
+
+// const myarr = [100,55,564,54]
+
+// function returnarray (getArray)
+// {
+//     return getArray[2];
+// }
+
+// first method 
+// console.log(returnarray(myarr));
+
+// second method
+// console.log(returnarray([100,500,763,656]))
