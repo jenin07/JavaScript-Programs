a6683674bc08726561ac0d7f329743d959712857
Jenin <110290773+jenin07@users.noreply.github.com>
2024-07-18 13:21:07 +0000

    filter

---
+++ iterators/filter.js
@@ -0,0 +1,32 @@
+// using filter we can retrun the single values of the array whereas in for each loop we can't work on single values
+
+const num = [1,23,3,211,434,3454,532,]
+// without parenthsis
+// const newnum = num.filter( (num) => num > 211)
+
+// with parenthsis
+// const newnum = num.filter( (num) => (num > 211))
+
+// with scope
+// const newnum = num.filter( (num) => 
+    // {
+    //     num > 211)
+    // }
+
+// console.log(newnum)
+
+
+
+// foreach loop id we want to return the value
+
+
+// const nnum = []
+// num.forEach ( (num) =>{
+//     if (num >211) {
+//         nnum.push(num);
+//     }
+// }
+// )
+
+// console.log(nnum);
+
