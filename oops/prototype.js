d5d2570acfbdf407f1f69092af4d0212d6bcb61c
Jenin <110290773+jenin07@users.noreply.github.com>
2024-08-02 18:16:10 +0000

    prototypes

---
+++ oops/prototype.js
@@ -0,0 +1,54 @@
+// let myname = "jenin";
+
+// console.log(myname.length);
+// console.log(myname.trueLength);
+
+
+let hero = ["spidy", "thor", "iromman"]
+
+let power  = {
+    thor: "hammer",
+    spidy: "web",
+    ironman: "blast",
+
+    spidyPower: function (){
+        console.log(`spidy power is ${this.spidy}`);
+        
+    }
+}
+
+// injecting a new function in object using prototype by which this function will be available in every object.
+Object.prototype.god = function(){
+    console.log("god is in every object");
+    
+}
+
+// injecting a new function in array using prototype by which this function will be available in every object.
+
+Array.prototype.hello = function() {
+    console.log(`hello ${power.thor}` );
+    
+}
+
+hero.hello(); //it will return the value as hero is an array datatype and we have added a new function in array's property with the help of prototype
+
+
+power.hello(); //it will not return the value as power is an object datatype and we have added a new function in array's property with the help of prototype so it won't work on object as object has higher piority than array. we can call a function that is created in object in a array datatype like below example
+
+// power.god();
+// hero.god()
+
+
+//inheritance
+
+const techer =  {
+    video: true
+}
+const support = {
+    avail: false
+}
+
+const tasup ={
+    fulltime: true,
+    __proto__: support
+}
