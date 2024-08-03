d5933081451432b24514072f44255e64976ea943
Jenin <110290773+jenin07@users.noreply.github.com>
2024-08-03 10:53:38 +0000

    static

--- oops/staticprop.js
+++ oops/staticprop.js
@@ -8,7 +8,7 @@ class user{
         
     };
 
-    // if we don't want to give access of createid to everyone then we can use static keyword before the function name
+    // if we don't want to give access of createid to everyone then we can use static keyword before the function name. The static keyword doesn't give access to it's inherited class also if we don't want to give the access.
     static createid(){
         return `123`
     }
