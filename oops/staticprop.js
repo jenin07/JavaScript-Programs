f3ed7fd3728d7db5403e9656b2cb34ff67b11e78
Jenin <110290773+jenin07@users.noreply.github.com>
2024-08-03 10:51:40 +0000

    static

---
+++ oops/staticprop.js
@@ -0,0 +1,20 @@
+class user{
+    constructor(username){
+        this.username = username;
+    }
+
+    logme(){
+        console.log(`username: ${this.username}`);
+        
+    };
+
+    // if we don't want to give access of createid to everyone then we can use static keyword before the function name
+    static createid(){
+        return `123`
+    }
+    
+    
+}
+
+const jenin = new user("jenin")
+console.log(jenin.createid());
