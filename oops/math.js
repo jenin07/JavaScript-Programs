d47163dae10f32492fb8f99fee5a2481541958fa
Jenin <110290773+jenin07@users.noreply.github.com>
2024-08-04 11:58:03 +0000

    changing properties of object

--- oops/math.js
+++ oops/math.js
@@ -1,3 +1,5 @@
+// in js we can't change the value of math pi because Because PI is a static property of Math, you always use it as Math.PI, rather than as a property of a Math object you created (Math is not a constructor) and in its properties writable, enumerable and congiurable all of them are false and even if we try to change the property of math.pi using object.defineproperty we can't change it as it is hardcoded in cpp language in depth.
+
 const desc = Object.getOwnPropertyDescriptor(Math, "PI")
 
 // console.log(desc);
