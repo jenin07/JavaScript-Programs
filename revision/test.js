0d14a1a03f978bb69dedf0c61a043f90617ca20e
Jenin <110290773+jenin07@users.noreply.github.com>
2024-09-03 15:44:31 +0000

    revision

---
+++ revision/test.js
@@ -0,0 +1,14 @@
+const accountName = "hdfc bank";
+const accountId = 6532;
+const accountEmail = "hdfcbank@gmail.com";
+const accountCity = "Delhi";
+accountState = "active"
+let account;
+
+// this is one way to print the details
+console.log(accountName); 
+
+// we can also print multiple values also using console tables
+
+console.table([accountId, accountEmail, accountCity, accountState, account])
+
